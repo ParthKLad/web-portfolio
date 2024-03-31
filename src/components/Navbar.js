6d8887bdf8368956bc6f718ab2be6be2baacdf3a
Parth <98051471+parthlad9@users.noreply.github.com>
2024-03-31 16:26:23 -0500

    further fixed nav and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,16 @@
 import React, { useState, useContext, useEffect } from 'react';
 import {
-  AppBar, Toolbar, IconButton, Box, Drawer, List, ListItem, ListItemIcon, ListItemText,
-  useMediaQuery, useTheme, CssBaseline, Tooltip, Typography
+  AppBar, Toolbar, Box, Typography, Button, useMediaQuery, useTheme, CssBaseline,
+  Drawer, List, ListItem, ListItemText
 } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import HomeIcon from '@mui/icons-material/HomeOutlined';
-import PersonIcon from '@mui/icons-material/PersonOutline';
-import WorkIcon from '@mui/icons-material/WorkOutline';
-import CodeIcon from '@mui/icons-material/CodeOutlined';
-import EmailIcon from '@mui/icons-material/EmailOutlined';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
 import { ThemeContext } from '../context/ThemeContext';
 
 function Navbar({ refs }) {
   const { themeType, toggleTheme } = useContext(ThemeContext);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [activeNav, setActiveNav] = useState('Home');
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-  const [activeNav, setActiveNav] = useState('Home');
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,114 +26,120 @@ function Navbar({ refs }) {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [refs.homeRef]);
 
+
+  const navItems = [
+    { name: 'Home', ref: refs.homeRef },
+    { name: 'About', ref: refs.aboutRef },
+    { name: 'Projects', ref: refs.projectsRef },
+    { name: 'Skills', ref: refs.skillsRef },
+    { name: 'Contact', ref: refs.contactsRef },
+  ];
+
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
   };
 
-  const navItems = [
-    { name: 'Home', ref: refs.homeRef, icon: <HomeIcon />, label: 'Home' },
-    { name: 'About', ref: refs.aboutRef, icon: <PersonIcon />, label: 'About' },
-    { name: 'Projects', ref: refs.projectsRef, icon: <WorkIcon />, label: 'Projects' },
-    { name: 'Skills', ref: refs.skillsRef, icon: <CodeIcon />, label: 'Skills' },
-    { name: 'Contact', ref: refs.contactsRef, icon: <EmailIcon />, label: 'Contact' },
-  ];
-
-  const handleNavItemClicked = (item) => {
-    setActiveNav(item.name);
-    if (item.ref && item.ref.current) {
-      item.ref.current.scrollIntoView({ behavior: 'smooth' });
+  const handleNavItemClicked = (name, ref) => {
+    setActiveNav(name);
+    if (ref && ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
     }
     handleDrawerToggle();
   };
+  
 
   return (
     <>
       <CssBaseline />
       <AppBar position="fixed" sx={{
-        background: activeNav === 'Home' ? 'transparent' : '#14CEDC',
+        background: 'transparent',
         backdropFilter: 'blur(10px)',
         boxShadow: 'none',
-        transition: 'background-color 0.3s',
-        color: theme.palette.getContrastText(theme.palette.background.default),
-        height: '56px',
-        justifyContent: 'space-between',
+        transition: 'all 0.3s',
       }}>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
+        <Toolbar sx={{ justifyContent: 'space-between', alignItems: 'center' }}>
+          <Typography variant="h6" sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', cursor: 'pointer' }}>
+            <Box onClick={() => handleNavItemClicked('Home', refs.homeRef)}>
+              <Typography
+                sx={{
+                  mr: 1, fontWeight: themeType === 'dark' ? 300 : 700,
+                  color: themeType === 'dark' ? '#0B83B3' : '#FFF',
+                  '&:hover': { color: themeType === 'dark' ? '#FFF' : '#0B83B3', },
+                }}
+              >
+                Parth
+              </Typography>
+              <Typography
+                sx={{
+                  fontWeight: themeType === 'dark' ? 700 : 300,
+                  color: themeType === 'dark' ? '#FFF' : '#0B83B3',
+                  '&:hover': { color: themeType === 'dark' ? '#0B83B3' : '#FFF', },
+                }}
+              >
+                Lad
+              </Typography>
+            </Box>
+          </Typography>
+
           {!isMobile && (
-            <Typography variant="h6" sx={{ flexGrow: 1, userSelect: 'none' }}>
-              <div className="name-container">
-                <span className="name-part parth" style={{ marginRight: '10px' }}>Parth</span>
-                <span className="name-part lad">Lad</span>
-              </div>
-            </Typography>
-          )}
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            {!isMobile && navItems.map((item, index) => (
-              <Tooltip key={index} title={item.label} placement="bottom">
-                <IconButton
-                  onClick={() => handleNavItemClicked(item)}
-                  color="inherit"
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              {navItems.map((item) => (
+                <Button
+                  key={item.name}
+                  onClick={() => handleNavItemClicked(item.name, item.ref)}
                   sx={{
                     mx: 2,
-                    borderRadius: '10px',
-                    '&:hover, &:focus': {
-                      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                    color: 'inherit',
+                    '&::after': {
+                      content: '""',
+                      position: 'absolute',
+                      width: activeNav === item.name ? '100%' : '0',
+                      height: '2px',
+                      bottom: 0,
+                      left: 0,
+                      bgcolor: '#0B83B3',
+                      transition: 'width 0.3s',
                     },
-                    ...(activeNav === item.name && {
-                      backgroundColor: 'rgba(255, 255, 255, 0.3)',
-                      boxShadow: 'inset 0 0 10px 5px rgba(0, 0, 0, 0.1)',
-                    }),
+                    '&:hover::after': {
+                      width: '100%',
+                    },
+                    '&:hover': {
+                      color: '#0B83B3',
+                    },
                   }}
                 >
-                  {item.icon}
-                </IconButton>
-              </Tooltip>
-            ))}
-            {!isMobile && (
-              <Tooltip title="Toggle light/dark theme">
-                <IconButton onClick={toggleTheme} color="inherit">
-                  {themeType === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}
-                </IconButton>
-              </Tooltip>
-            )}
-          </Box>
+                  {item.name}
+                </Button>
+              ))}
+              <Button onClick={toggleTheme} sx={{ ml: 2 }}>
+                {themeType === 'dark' ? <Typography>Light</Typography> : <Typography>Dark</Typography>}
+              </Button>
+            </Box>
+          )}
+
           {isMobile && (
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="end"
-              onClick={handleDrawerToggle}
-              sx={{
-                ml: 'auto', // ensures the icon button is aligned to the right on mobile
-                ...(drawerOpen && {
-                  // Apply the rotate animation when drawer is open
-                  animation: 'rotate-open 0.5s forwards',
-                }),
-                ...(!drawerOpen && {
-                  // Apply the rotate animation when drawer is closed
-                  animation: 'rotate-close 0.5s forwards',
-                }),
-              }}
-            >
-              {drawerOpen ? <CloseIcon /> : <MenuIcon />}
-            </IconButton>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Button onClick={toggleTheme}>
+                {themeType === 'dark' ? <Typography>Light</Typography> : <Typography>Dark</Typography>}
+              </Button>
+              <Button onClick={handleDrawerToggle} sx={{ ml: 2 }}>
+                <Typography sx={{ transform: drawerOpen ? 'rotate(45deg)' : 'rotate(0)' }}>
+                  {drawerOpen ? '✕' : '☰'}
+                </Typography>
+              </Button>
+            </Box>
           )}
         </Toolbar>
       </AppBar>
-      
+
       {isMobile && (
-        <Drawer anchor='right' open={drawerOpen} onClose={handleDrawerToggle}>
+        <Drawer anchor='right' open={drawerOpen} onClose={handleDrawerToggle} sx={{ '.MuiDrawer-paper': { width: '75%' } }}>
           <List>
-            {navItems.map((item, index) => (
-              <ListItem button key={index} onClick={() => handleNavItemClicked(item)}>
-                <ListItemIcon>{item.icon}</ListItemIcon>
-                <ListItemText primary={item.label} />
+            {navItems.map((item) => (
+              <ListItem button key={item.name} onClick={() => handleNavItemClicked(item.name, item.ref)}>
+                <ListItemText primary={item.name} sx={{ textAlign: 'center' }} />
               </ListItem>
             ))}
-            <ListItem button onClick={toggleTheme}>
-              <ListItemIcon>{themeType === 'dark' ? <LightModeIcon /> : <DarkModeIcon />}</ListItemIcon>
-              <ListItemText primary="Toggle" />
-            </ListItem>
           </List>
         </Drawer>
       )}
