f9efe2f6d3e95234e7142a4b36fcd07c6b340cfa
iparthprod <parth.lad@protonmail.com>
2024-04-03 18:55:08 -0500

    test

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,187 +1,123 @@
-import React, { useState } from 'react';
-import { Box, Typography, TextField, Button, styled, Paper, IconButton, useTheme, ThemeProvider, createTheme, Grow } from '@mui/material';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, TextField, Button, Paper, IconButton, Grow, useTheme as useMuiTheme } from '@mui/material';
+import { styled } from '@mui/system';
 import { Send, LinkedIn, GitHub } from '@mui/icons-material';
-import ReCAPTCHA from "react-google-recaptcha";
-// Google reCAPTCHA site key
+import ReCAPTCHA from 'react-google-recaptcha';
+
+// Assuming your ThemeContext is set up as described
+import { useTheme } from '../context/ThemeContext'; // Adjust the path to where your ThemeContext is defined
+
 const RECAPTCHA_SITE_KEY = '6LeaCa0pAAAAAEHdxAyha8E_sdNkeeOXvXfhwDRy';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#14CEDC',
-    },
-    secondary: {
-      main: '#e91e63',
-    },
-    text: {
-      primary: '#212121',
-      secondary: '#757575',
-    },
-    background: {
-      paper: '#fff',
-      default: '#fafafa',
-    },
-  },
-});
-
-const StyledPaper = styled(Paper)({
-  display: 'flex',
-  flexDirection: 'row',
-  borderRadius: '8px',
-  overflow: 'hidden',
-  boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
-  position: 'relative',
-  width: '80%',
-  height: 'auto',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  '@media (max-width:600px)': {
-    flexDirection: 'column',
-    width: '100%',
-  },
-});
-
-const StyledSection = styled(Box)({
-  padding: '16px 32px 32px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'flex-start',
-  zIndex: 1,
-  '@media (max-width:600px)': {
-    width: '100%',
-    '&:first-child': {
-      marginBottom: '16px',
-    },
-  },
-  ':first-of-type': {
-    width: '60%',
-  },
-  ':last-child': {
-    width: '40%',
-  },
-});
-
-const CustomTextField = styled(TextField)(({ theme }) => ({
-  '& label.Mui-focused': {
-    color: theme.palette.primary.main,
-  },
-  '& .MuiInput-underline:before': {
-    borderBottomColor: theme.palette.text.secondary,
-  },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: theme.palette.primary.main,
-  },
-  '& label': {
-    color: theme.palette.text.secondary,
-  },
-  '& input': {
-    color: theme.palette.text.primary,
-  },
-  marginBottom: '16px',
-}));
-
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Here, handle the form submission to Netlify or your server
-    console.log({ email, subject, message, recaptchaToken });
-    // Remember to include logic for handling the recaptchaToken with your submission
-  };
-
-const CustomButton = styled(Button)({
-  color: theme.palette.getContrastText('#000'),
-  backgroundColor: '#14CEDC', // Background color changed to black
-  '&:hover': {
-    backgroundColor: 'rgba(0, 0, 0, 0.8)', // Darker on hover
-  },
-});
-
-const SocialIconsRow = styled(Box)({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingTop: '16px',
-  gap: '16px',
-});
-
-const ContactInfoItem = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  gap: '8px',
-  marginBottom: '16px',
-  '@media (max-width:600px)': {
-    alignItems: 'flex-start',
-    textAlign: 'left',
-    width: '100%',
-  },
-}));
-
 const ContactForm = () => {
+  const { themeType } = useTheme(); // Use your ThemeContext
+  const muiTheme = useMuiTheme(); // MUI Theme for styles
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
-  const [recaptchaToken, setRecaptchaToken] = useState(''); // Correctly define the useState hook for ReCAPTCHA token
+  const [recaptchaToken, setRecaptchaToken] = useState('');
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here, handle the form submission to Netlify or your server
     console.log({ email, subject, message, recaptchaToken });
-    // Remember to include logic for handling the recaptchaToken with your submission
+    // Handle your form submission logic here
   };
 
+  // Define styled components
+  const StyledPaper = styled(Paper)({
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: '8px',
+    overflow: 'hidden',
+    boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
+    position: 'relative',
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: muiTheme.palette.background.paper,
+    '@media (max-width:600px)': {
+      flexDirection: 'column',
+      width: '100%',
+    },
+  });
+
+  const StyledSection = styled(Box)({
+    padding: '16px 32px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    zIndex: 1,
+    width: '100%', // Adjust based on your layout needs
+    
+  });
+  
+
+  const SocialIconsRow = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: '16px',
+    gap: '16px',
+  });
+
+  const ContactInfoItem = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '8px',
+    marginBottom: '16px',
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <Grow in={true} style={{ transformOrigin: '0 0 0' }} timeout={1000}>
-        <Box sx={{ maxWidth: '1450px', margin: 'auto', width: '100%' }}>
-          <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
-            Contact
-          </Typography>
-          <Paper component="form" onSubmit={handleSubmit} method="POST" sx={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <input type="hidden" name="form-name" value="contact" />
-            <TextField 
-              fullWidth 
-              label="Email" 
-              variant="standard" 
-              name="email" 
-              value={email} 
-              onChange={(e) => setEmail(e.target.value)} 
-              sx={{ marginBottom: '20px' }}
-            />
-            <TextField 
-              fullWidth 
-              label="Subject" 
-              variant="standard" 
-              name="subject" 
-              value={subject} 
-              onChange={(e) => setSubject(e.target.value)}
-              sx={{ marginBottom: '20px' }}
-            />
-            <TextField 
-              fullWidth 
-              label="Message" 
-              variant="standard" 
-              multiline 
-              rows={4} 
-              name="message" 
-              value={message} 
-              onChange={(e) => setMessage(e.target.value)}
-              sx={{ marginBottom: '20px' }}
-            />
-            <ReCAPTCHA 
-              sitekey={RECAPTCHA_SITE_KEY} 
-              onChange={setRecaptchaToken} // Make sure this matches the useState setter name
-              sx={{ margin: '20px 0' }}
-            />
-            <Button variant="contained" endIcon={<Send />} type="submit" sx={{ backgroundColor: '#14CEDC', '&:hover': { backgroundColor: 'rgba(20, 206, 220, 0.8)' } }}>
+    <Grow in={checked} style={{ transformOrigin: '0 0 0' }} {...(checked ? { timeout: 1000 } : {})}>
+      <Box sx={{ maxWidth: '1450px', maxHeight: '522px', margin: 'auto', width: '100%' }}>
+        <Typography variant="h4" gutterBottom style={{ textAlign: 'center' }}>
+          Contact
+        </Typography>
+        <StyledPaper component="form" onSubmit={handleSubmit} method="POST" data-netlify="true" data-netlify-recaptcha="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <StyledSection>
+            <ContactInfoItem>
+              <Typography variant="h5" gutterBottom noWrap>
+                Get in touch
+              </Typography>
+              <Typography variant="h6" gutterBottom noWrap>
+                <strong>🙋‍♂️</strong> Parth Lad
+              </Typography>
+              <Typography variant="h6" gutterBottom noWrap>
+                <strong>📧</strong> parth.lad@protonmail.com
+              </Typography>
+            </ContactInfoItem>
+            <SocialIconsRow>
+              <IconButton href="https://www.linkedin.com/in/parthlad01" target="_blank" rel="noopener" color="primary">
+                <LinkedIn />
+              </IconButton>
+              <IconButton href="https://github.com/parthlad9" target="_blank" rel="noopener" color="primary">
+                <GitHub />
+              </IconButton>
+            </SocialIconsRow>
+          </StyledSection>
+          <StyledSection>
+            <TextField fullWidth label="Email" variant="standard" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <TextField fullWidth label="Subject" variant="standard" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
+            <TextField fullWidth label="Message" variant="standard" multiline rows={4} name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <br></br>
+            <br></br>
+            <ReCAPTCHA sitekey={RECAPTCHA_SITE_KEY} onChange={setRecaptchaToken} />
+            <Button variant="contained" endIcon={<Send />} type="submit" sx={{ marginTop: '20px' }}>
               Send
             </Button>
-          </Paper>
-        </Box>
-      </Grow>
-    </ThemeProvider>
+          </StyledSection>
+        </StyledPaper>
+      </Box>
+    </Grow>
   );
 };
 
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,56 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useTheme } from '@mui/material/styles';
 
 const Footer = () => {
     const theme = useTheme();
     const year = new Date().getFullYear();
-    const name = "Parth"; 
+    const name = "Parth";
 
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        const toggleVisibility = () => {
-            if (window.pageYOffset > 300) {
-                setIsVisible(true);
-            } else {
-                setIsVisible(false);
-            }
-        };
-
-        window.addEventListener("scroll", toggleVisibility);
-
-        return () => window.removeEventListener("scroll", toggleVisibility);
-    }, []);
+    // Inline style for the footer
+    const footerStyle = {
+        justifyContent: 'center',
+        background: 'transparent',
+        alignItems: 'center',
+        color: theme.palette.text.primary,
+        fontFamily: 'Arial, sans-serif',
+        fontSize: '12px',
+        padding: '20px 0', // Add some padding for aesthetic spacing
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+        width: '100%', // Ensure the footer takes up the full width
+    };
 
     return (
-        <footer style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: theme.palette.text.primary,
-            fontFamily: 'Arial, sans-serif',
-            fontSize: '12px',
-            padding: '1px 1px 0px 1px',
-            // backgroundColor: theme.palette.background.default, // This line is commented out to remove background color
-            borderBottom: `0px solid ${theme.palette.divider}`,
-            display: 'flex',
-            flexDirection: 'column',
-            transform: isVisible ? 'translateY(0)' : 'translateY(100px)',
-            background: 'transparent',
-            opacity: isVisible ? 1 : 0,
-            transition: 'all 0.5s ease-out',
-        }}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            
-
+        <footer style={footerStyle}>
             <p>© {year} All rights reserved. Made with love ❤️ by {name}</p>
         </footer>
     );
 };
 
 export default Footer;
-
