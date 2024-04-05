6e2c8da2d6965dc51e0f997f70d0df9d43b5f66d
Parth <98051471+parthlad9@users.noreply.github.com>
2024-04-05 00:34:19 -0500

    test dialog

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,6 +3,8 @@ import { Box, Typography, TextField, Button, Grow, Grid } from '@mui/material';
 import { Send } from '@mui/icons-material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 
 const ContactForm = () => {
   const theme = useTheme();
@@ -11,6 +13,8 @@ const ContactForm = () => {
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
   const matches = useMediaQuery(theme.breakpoints.up('md'));
+  const [open, setOpen] = useState(false);
+
 
   useEffect(() => {
     setChecked(true);
@@ -32,23 +36,21 @@ const ContactForm = () => {
           'Content-Type': 'application/x-www-form-urlencoded',
         },
       });
-    
+  
       if (response.ok) {
         console.log('Form submitted successfully!');
-        alert('Form submitted successfully!');
+        setOpen(true);
         setEmail('');
         setSubject('');
         setMessage('');
       } else {
         console.error('Form submission failed!', response.status);
-        alert('Form submission failed!');
       }
     } catch (error) {
       console.error('An error occurred:', error);
-      alert('An error occurred: ' + error);
-    }    
+    }
   };
-
+  
   return (
     <>
       <Typography variant="h4" gutterBottom textAlign="center">
@@ -56,81 +58,89 @@ const ContactForm = () => {
       </Typography>
       <br></br>
       <Grow in={checked} style={{ transformOrigin: '0 0 0' }}>
-      <Box sx={{ maxWidth: matches ? '62%' : '90%', height :matches ? '400px' : '600', margin: 'auto', backgroundColor: theme.palette.mode === 'dark' ? '#252424' : '#fff',borderRadius: '16px'  }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={5}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', height: '100%', paddingLeft: matches ? '10%' : '5%' }}>
-            <br></br>
-            <br></br>
-            <br></br>
-              <Typography variant="h5" gutterBottom sx={{ marginTop: matches ? '-50%' : '0%' }}>
-               📝 Get in touch
-              </Typography>
+        <Box sx={{ maxWidth: matches ? '62%' : '90%', height: matches ? '400px' : '600', margin: 'auto', backgroundColor: theme.palette.mode === 'dark' ? '#252424' : '#fff', borderRadius: '16px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={5}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', height: '100%', paddingLeft: matches ? '10%' : '5%' }}>
+                <br></br>
+                <br></br>
+                <br></br>
+                <Typography variant="h5" gutterBottom sx={{ marginTop: matches ? '-50%' : '0%' }}>
+                  📝 Get in touch
+                </Typography>
+                <br></br>
+                <br></br>
+                <Typography variant="h6" gutterBottom>
+                  👋: Parth Lad
+                </Typography>
+                <Typography variant="h6" gutterBottom>
+                  📧: Parth.lad@protonmail.com
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={7}>
               <br></br>
               <br></br>
-              <Typography variant="h6" gutterBottom>
-                👋: Parth Lad
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                📧: Parth.lad@protonmail.com
-              </Typography>
-            </Box>
+              <Box sx={{ paddingLeft: '30px', paddingRight: '50px', paddingBottom: '50px' }}>
+                <form onSubmit={handleSubmit} method="POST" data-netlify="true">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <TextField
+                      fullWidth
+                      label="Email"
+                      variant="outlined"
+                      name="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <TextField
+                      fullWidth
+                      label="Subject"
+                      variant="outlined"
+                      name="subject"
+                      value={subject}
+                      onChange={(e) => setSubject(e.target.value)}
+                  />
+                  <TextField
+                      fullWidth
+                      label="Message"
+                      variant="outlined"
+                      multiline
+                      rows={4}
+                      name="message"
+                      value={message}
+                      onChange={(e) => setMessage(e.target.value)}
+                  />
+                  <Button
+                      variant="contained"
+                      endIcon={<Send />}
+                      type="submit"
+                      sx={{ 
+                        marginTop: '20px',
+                        backgroundColor: '#15CEDC', // Change this to your desired color
+                        color: '#ffffff',
+                        '&:hover': {
+                          backgroundColor: '#0B83B3', // Change this to your desired hover color
+                        },
+                      }}
+                  >
+                      Send
+                  </Button>
+                </form>
+              </Box>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={7}>
-          <br></br>
-          <br></br>
-            <Box sx={{ paddingLeft: '30px', paddingRight: '50px', paddingBottom: '50px' }}>
-              <form onSubmit={handleSubmit} method="POST" data-netlify="true">
-                <input type="hidden" name="form-name" value="contact" />
-                <TextField
-                    fullWidth
-                    label="Email"
-                    variant="outlined"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="Subject"
-                    variant="outlined"
-                    name="subject"
-                    value={subject}
-                    onChange={(e) => setSubject(e.target.value)}
-                />
-                <TextField
-                    fullWidth
-                    label="Message"
-                    variant="outlined"
-                    multiline
-                    rows={4}
-                    name="message"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                />
-                <Button
-                    variant="contained"
-                    endIcon={<Send />}
-                    type="submit"
-                    sx={{ 
-                      marginTop: '20px',
-                      backgroundColor: '#15CEDC', // Change this to your desired color
-                      color: '#ffffff',
-                      '&:hover': {
-                        backgroundColor: '#0B83B3', // Change this to your desired hover color
-                      },
-                    }}
-                >
-                    Send
-                </Button>
-              </form>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
-    </Grow>
+        </Box>
+      </Grow>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogContent>
+          <Box sx={{ maxWidth: '90%', height: '400px', margin: 'auto', backgroundColor: theme.palette.mode === 'dark' ? '#252424' : '#fff', borderRadius: '16px' }}>
+            Form submitted successfully!
+          </Box>
+        </DialogContent>
+      </Dialog>
     </>
   );
+  
 };
 
 export default ContactForm;
