bc340739d152643cc4de26a582ed917e6cf9a203
iparthprod <parth.lad@protonmail.com>
2024-04-04 20:18:25 -0500

    test

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,16 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Box, Typography, TextField, Button, Grow } from '@mui/material';
 import { Send } from '@mui/icons-material';
 
-import ReCAPTCHA from 'react-google-recaptcha';
-import { useTheme } from '@mui/system';
-
 const ContactForm = () => {
-  const theme = useTheme();
   const [checked, setChecked] = useState(false);
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [message, setMessage] = useState('');
-  const [recaptchaToken, setRecaptchaToken] = useState('');
 
   useEffect(() => {
     setChecked(true);
@@ -23,7 +18,6 @@ const ContactForm = () => {
     formData.append('email', email);
     formData.append('subject', subject);
     formData.append('message', message);
-    formData.append('g-recaptcha-response', recaptchaToken);
   
     try {
       const response = await fetch('/', {
@@ -36,6 +30,10 @@ const ContactForm = () => {
   
       if (response.ok) {
         console.log('Form submitted successfully!');
+        // Optionally reset form fields here
+        setEmail('');
+        setSubject('');
+        setMessage('');
       } else {
         console.error('Form submission failed!');
       }
@@ -46,20 +44,15 @@ const ContactForm = () => {
 
   return (
     <Grow in={checked} style={{ transformOrigin: '0 0 0' }}>
-      <Box sx={{ maxWidth: '600px', margin: 'auto', backgroundColor: theme.palette.background.paper }}>
+      <Box sx={{ maxWidth: '600px', margin: 'auto' }}>
         <Typography variant="h4" gutterBottom textAlign="center">
           Contact
         </Typography>
-        <form onSubmit={handleSubmit} method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <form onSubmit={handleSubmit} method="POST" data-netlify="true">
           <input type="hidden" name="form-name" value="contact" />
           <TextField fullWidth label="Email" variant="outlined" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <TextField fullWidth label="Subject" variant="outlined" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
           <TextField fullWidth label="Message" variant="outlined" multiline rows={4} name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-          <ReCAPTCHA
-            sitekey="6LeaCa0pAAAAAEHdxAyha8E_sdNkeeOXvXfhwDRy" 
-            onChange={setRecaptchaToken}
-            style={{ margin: '10px 0' }}
-          />
           <Button variant="contained" endIcon={<Send />} type="submit">
             Send
           </Button>
@@ -70,4 +63,3 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
-
