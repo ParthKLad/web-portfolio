25a53efa938bd448143ad40351ea2d2fffc87e48
Parth <98051471+parthlad9@users.noreply.github.com>
2024-04-04 23:59:10 -0500

    test

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -4,7 +4,6 @@ import { Send } from '@mui/icons-material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
-
 const ContactForm = () => {
   const theme = useTheme();
   const [checked, setChecked] = useState(false);
@@ -39,80 +38,86 @@ const ContactForm = () => {
         setEmail('');
         setSubject('');
         setMessage('');
+        // Optionally, provide user feedback here
       } else {
         console.error('Form submission failed!', response.status);
+        // Optionally, provide user feedback here
       }
     } catch (error) {
       console.error('An error occurred:', error);
+      // Optionally, provide user feedback here
     }
   };
 
   return (
     <Grow in={checked} style={{ transformOrigin: '0 0 0' }}>
-          <Box sx={{ maxWidth: matches ? '62%' : '90%', height :matches ? '400px' : '600' ,margin: 'auto', backgroundColor: theme.palette.mode === 'dark' ? '#252424' : '#fff' }}>
-      <Grid container spacing={2}>
-      <Grid item xs={12} md={5}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', height: '100%',paddingLeft: matches ? '50%' : '10%' 
- }}>
-        <Typography variant="h4" gutterBottom sx={{ position: 'relative', right: matches ? '50%' : '0%', marginTop: matches ? '-50%' : '0%' }}>
-        Get in touch 
-      </Typography>
-      <br></br>
-      <br></br>
-      <Typography variant="h6" gutterBottom sx={{ position: 'relative', right: matches ? '50%' : '0%' }}>
-        👋: Parth Lad
-      </Typography>
-      <Typography variant="h6" gutterBottom sx={{ position: 'relative', right: matches ? '50%' : '0%' }}>
-        📧: Parth.lad@protonmail.com
-      </Typography>
+      <Box sx={{ maxWidth: matches ? '62%' : '90%', height :matches ? '400px' : '600', margin: 'auto', backgroundColor: theme.palette.mode === 'dark' ? '#252424' : '#fff' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={5}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', height: '100%', paddingLeft: matches ? '10%' : '5%' }}>
+              <Typography variant="h4" gutterBottom sx={{ marginTop: matches ? '-50%' : '0%' }}>
+                Get in touch
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                👋: Parth Lad
+              </Typography>
+              <Typography variant="h6" gutterBottom>
+                📧: Parth.lad@protonmail.com
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={7}>
+            <Box sx={{ paddingLeft: '30px', paddingRight: '50px', paddingBottom: '50px' }}>
+              <form onSubmit={handleSubmit} method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
+                <TextField
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    fullWidth
+                    label="Subject"
+                    variant="outlined"
+                    name="subject"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                />
+                <TextField
+                    fullWidth
+                    label="Message"
+                    variant="outlined"
+                    multiline
+                    rows={4}
+                    name="message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <Button
+                    variant="contained"
+                    endIcon={<Send />}
+                    type="submit"
+                    sx={{ 
+                      marginTop: '20px',
+                      backgroundColor: '#15CEDC', // Change this to your desired color
+                      color: '#ffffff',
+                      '&:hover': {
+                        backgroundColor: '#0B83B3', // Change this to your desired hover color
+                      },
+                    }}
+                >
+                    Send
+                </Button>
+              </form>
+            </Box>
+          </Grid>
+        </Grid>
       </Box>
-    </Grid>
-    <Grid item xs={12} md={7}>
-    <Box sx={{ paddingLeft: '30px', paddingRight: '50px', paddingBottom: '50px' }}>
-        <form onSubmit={handleSubmit} method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-            <br></br>
-            <TextField
-                fullWidth
-                label="Email"
-                variant="outlined"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-                fullWidth
-                label="Subject"
-                variant="outlined"
-                name="subject"
-                value={subject}
-                onChange={(e) => setSubject(e.target.value)}
-            />
-            <TextField
-                fullWidth
-                label="Message"
-                variant="outlined"
-                multiline
-                rows={4}
-                name="message"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-            />
-            <Button
-                variant="contained"
-                endIcon={<Send />}
-                type="submit"
-                sx={{ marginTop: '20px' }}
-            >
-                Send
-            </Button>
-        </form>
-    </Box>
-</Grid>
-
-</Grid>
-</Box>
     </Grow>
   );
-}
+};
+
 export default ContactForm;
