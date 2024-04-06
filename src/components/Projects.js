07889c57ec26f83721c1889dd78806f2a76efdfc
Parth <98051471+parthlad9@users.noreply.github.com>
2024-04-05 23:17:42 -0500

    test

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Grid, Paper, Typography, Dialog, DialogTitle, DialogContent, DialogActions, Button, IconButton,  useTheme,useMediaQuery ,Card,
-  CardMedia,Box, CardActionArea ,
+  CardMedia,Box ,
   CardContent,} from '@mui/material';
-import CloseIcon from '@mui/icons-material/Close';
-
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import CardActionArea from '@mui/material/CardActionArea';
+
+import CloseIcon from '@mui/icons-material/Close'; // Import CloseIcon
+
 
 // Assuming these imports are correct
 import Sharewell from '../images/Sharewell/sharewell_login.png';
@@ -14,83 +16,19 @@ import Sharewell_tool from '../images/Sharewell/sharewell_tool.png';
 
 const projects = [
   {
-    title: 'Sharewell HDD',
+    title: 'Coming Soon ',
     images: [
         Sharewell,
         Sharewell_dash,
         Sharewell_tool   
     ],
    // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
+   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor quis dolor volutpat convallis. Vivamus eleifend ex vel felis cursus aliquam. Phasellus tempus dolor ac tincidunt cursus. Sed convallis sagittis libero, eu porta ligula consequat sed.",
   
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
+  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies justo ac metus suscipit congue. Donec euismod faucibus nibh, ut varius risus fermentum a. Proin purus nulla, hendrerit quis egestas vel, imperdiet quis felis. Suspendisse suscipit sapien eget nisi molestie egestas. Mauris quis lectus laoreet, pulvinar diam vitae, sodales massa. Mauris a risus et ipsum venenatis sodales id id nibh. Vivamus fermentum venenatis tortor, consectetur tempor ex vulputate ac. Etiam accumsan purus ac ipsum auctor, sed scelerisque libero posuere. Integer malesuada purus ex, sit amet venenatis eros mollis eget. Donec lobortis risus diam, eu viverra est mattis eu. Aenean vehicula felis vel ligula facilisis, vel dignissim mi sollicitudin. Nullam id ligula sapien. Suspendisse potenti. Morbi quis felis non dui scelerisque gravida. Curabitur ultricies vehicula ante sit amet fermentum.Proin ornare tortor quam, vel volutpat lectus aliquet in. Vestibulum vitae risus posuere, tincidunt enim a, dictum magna. Donec vel auctor elit, non porttitor dui. Pellentesque non bibendum nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras iaculis turpis laoreet nibh condimentum blandit. Nam tincidunt, ligula ut convallis eleifend, tortor mi sagittis nunc, sit amet aliquam eros nulla vel tellus. Curabitur eu condimentum dolor. Aliquam varius massa vel mi ultricies, sollicitudin cursus turpis varius. Cras molestie lobortis nisl a vestibulum. Nunc consectetur ipsum et dignissim feugiat. Ut non lectus ac enim pretium lobortis volutpat id ante."
   },
 
-  {
-    title: 'Sharewell HDD',
-    images: [
-        Sharewell,
-        Sharewell_dash,
-        Sharewell_tool   
-    ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
   
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
-  },
-
-  {
-    title: 'Sharewell HDD',
-    images: [
-        Sharewell,
-        Sharewell_dash,
-        Sharewell_tool   
-    ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
-  
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
-  },
-
-  {
-    title: 'Sharewell HDD',
-    images: [
-        Sharewell,
-        Sharewell_dash,
-        Sharewell_tool   
-    ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
-  
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
-  },
-
-  {
-    title: 'Sharewell HDD',
-    images: [
-        Sharewell,
-        Sharewell_dash,
-        Sharewell_tool   
-    ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
-  
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
-  },
-
-  {
-    title: 'Sharewell HDD',
-    images: [
-        Sharewell,
-        Sharewell_dash,
-        Sharewell_tool   
-    ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque semper aliquam felis, non volutpat felis.",
-  
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a...Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum malesuada pellentesque. Vivamus pretium felis eu ipsum fringilla condimentum. Integer tincidunt, nunc eget rhoncus semper, eros justo molestie arcu, a."
-  },
-
 
   
 
@@ -166,10 +104,10 @@ function Projects() {
           fullScreen={fullScreen}
         >
           <IconButton onClick={() => setOpen(false)} sx={{ position: 'absolute', right: 8, top: 8, zIndex: 2, color: 'white' }}>
-          <CloseIcon />
+            <CloseIcon />
           </IconButton>
           <DialogContent sx={{ overflowY: 'hidden' }}>
-            <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex - 1 + selectedProject.images.length) % selectedProject.images.length)} sx={{ position: 'absolute', left: 8, top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
+            <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex - 1 + selectedProject.images.length) % selectedProject.images.length)} sx={{ position: 'absolute', left: 8, top: fullScreen ? '50%' : '30%', transform: 'translateY(-50%)', zIndex: 2 }}>
               <ArrowBackIosNewIcon />
             </IconButton>
             <CardMedia
@@ -183,17 +121,17 @@ function Projects() {
               </Typography>
               <Typography variant="body2" color="text.secondary">
                 {selectedProject.fullDesc}
-                </Typography>
-              </CardContent>
-              <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex + 1) % selectedProject.images.length)} sx={{ position: 'absolute', right: 8, top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
-                <ArrowForwardIosIcon />
-              </IconButton>
-            </DialogContent>
-          </Dialog>
-        )}
-      </Container>
-    );
-  }
+              </Typography>
+            </CardContent>
+            <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex + 1) % selectedProject.images.length)} sx={{ position: 'absolute', right: 8, top: fullScreen ? '50%' : '30%', transform: 'translateY(-50%)', zIndex: 2 }}>
+              <ArrowForwardIosIcon />
+            </IconButton>
+          </DialogContent>
+        </Dialog>
+      )}
+    </Container>
+  );
   
-  export default Projects;
-  
+}
+
+export default Projects;
