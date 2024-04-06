016d4a4c73db03ca467d03183c15b8b379d0fd7c
Parth <98051471+parthlad9@users.noreply.github.com>
2024-04-06 00:41:43 -0500

    test ?

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,17 +21,24 @@ const projects = [
         Sharewell_dash,
         Sharewell_tool   
     ],
-   // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
-   shortDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget dolor quis dolor volutpat convallis. Vivamus eleifend ex vel felis cursus aliquam. Phasellus tempus dolor ac tincidunt cursus. Sed convallis sagittis libero, eu porta ligula consequat sed.",
-  
-  fullDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies justo ac metus suscipit congue. Donec euismod faucibus nibh, ut varius risus fermentum a. Proin purus nulla, hendrerit quis egestas vel, imperdiet quis felis. Suspendisse suscipit sapien eget nisi molestie egestas. Mauris quis lectus laoreet, pulvinar diam vitae, sodales massa. Mauris a risus et ipsum venenatis sodales id id nibh. Vivamus fermentum venenatis tortor, consectetur tempor ex vulputate ac. Etiam accumsan purus ac ipsum auctor, sed scelerisque libero posuere. Integer malesuada purus ex, sit amet venenatis eros mollis eget. Donec lobortis risus diam, eu viverra est mattis eu. Aenean vehicula felis vel ligula facilisis, vel dignissim mi sollicitudin. Nullam id ligula sapien. Suspendisse potenti. Morbi quis felis non dui scelerisque gravida. Curabitur ultricies vehicula ante sit amet fermentum.Proin ornare tortor quam, vel volutpat lectus aliquet in. Vestibulum vitae risus posuere, tincidunt enim a, dictum magna. Donec vel auctor elit, non porttitor dui. Pellentesque non bibendum nulla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras iaculis turpis laoreet nibh condimentum blandit. Nam tincidunt, ligula ut convallis eleifend, tortor mi sagittis nunc, sit amet aliquam eros nulla vel tellus. Curabitur eu condimentum dolor."
-  },
+    // link: 'https://parthlad9.github.io/Beastly-Ranked-Movies/',
+    shortDesc: "In a Project Management course, my team and I developed a Flask-based application for Sharewell to enhance their drilling tools’ efficiency. We analyzed Sharewell’s challenges, integrated advanced drilling data, and utilized a robust tech stack including SQL, JavaScript, Python, Flask, Express, and Node.js. We deployed our application on AWS EC2 and used AWS RDS for our SQL database. Our project management was facilitated by Jira and GitHub, and our solution significantly enhanced Sharewell’s operational decision-making and strategic planning.",
+    fullDesc: [
+      { title: "Introduction", description: "In a Project Management course, my team and I embarked on a mission to develop a custom Flask-based application for Sharewell. Our goal was to enhance the precision and efficiency of their drilling tools." },
+      { title: "Problem Analysis", description: "We conducted a thorough analysis of Sharewell's operational challenges. This helped us understand the issues at hand and guided our solution development process." },
+      { title: "Solution Development", description: "We integrated advanced downhole drilling data into our application. This allowed us to address the identified challenges effectively." },
+      { title: "Technology Stack", description: "Our project utilized a robust set of technologies including SQL, JavaScript, Python, Flask, Express, and Node.js. This comprehensive tech stack enabled us to perform in-depth data analysis and develop a high-performance application." },
+      { title: "Deployment", description: "We deployed both the backend and frontend servers on AWS EC2 instances. For our SQL database, we used AWS RDS, ensuring a scalable and reliable database solution." },
+      { title: "Project Management & Version Control", description: "We used Jira and GitHub for effective project management and version control. These tools ensured smooth collaboration among team members and efficient progress tracking." },
+      { title: "Outcome", description: "Our solution provided a more detailed and accurate evaluation of long-term data trends. This significantly enhanced operational decision-making and strategic planning for Sharewell." }
+    ]
+  }
+]
 
-  
 
-  
 
-];
+
+
 
 function Projects() {
   const [open, setOpen] = useState(false);
@@ -83,50 +90,74 @@ function Projects() {
         {projects.map((project, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
             <Card onClick={() => { setSelectedProject(project); setCurrentImageIndex(0); setOpen(true); }} sx={{ cursor: 'pointer' }}>
-              <CardActionArea>
-                <CardMedia component="img" height="140" image={project.images[0]} alt={project.title} />
-                <CardContent>
-                  <Typography gutterBottom variant="h5">{project.title}</Typography>
-                  <Typography variant="body2" color="text.secondary">{project.shortDesc}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
+            <CardActionArea>
+              <CardMedia component="img" height="200" image={project.images[0]} alt={project.title} />
+              <CardContent>
+                <Typography gutterBottom variant="h5">{project.title}</Typography>
+                <Typography variant="body2" color="text.secondary">{project.shortDesc}</Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
           </Grid>
         ))}
       </Grid>
       {selectedProject && (
-        <Dialog
-        open={open}
-        onClose={() => setOpen(false)}
-        fullWidth
-        maxWidth="md"
-        fullScreen={fullScreen}
-      >
-        <IconButton onClick={() => setOpen(false)} sx={{ position: 'absolute', right: 8, top: 8, zIndex: 2, color: 'white' }}>
-          <CloseIcon />
-        </IconButton>
-          <DialogContent sx={{ overflowY: 'hidden' }}>
-            <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex - 1 + selectedProject.images.length) % selectedProject.images.length)} sx={{ position: 'absolute', left: 8, top: fullScreen ? '50%' : '30%', transform: 'translateY(-50%)', zIndex: 2 }}>
-              <ArrowBackIosNewIcon />
-            </IconButton>
-            <CardMedia
-              component="img"
-              image={selectedProject.images[currentImageIndex]}
-              alt={`Image ${currentImageIndex + 1}`}
-            />
-            <CardContent sx={{ paddingTop: theme.spacing(2) }}>
-              <Typography gutterBottom variant="h5" component="div">
-                {selectedProject.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {selectedProject.fullDesc}
-              </Typography>
-            </CardContent>
-            <IconButton onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex + 1) % selectedProject.images.length)} sx={{ position: 'absolute', right: 8, top: fullScreen ? '50%' : '30%', transform: 'translateY(-50%)', zIndex: 2 }}>
-              <ArrowForwardIosIcon />
-            </IconButton>
-          </DialogContent>
-        </Dialog>
+   <Dialog
+   open={open}
+   onClose={() => setOpen(false)}
+   fullWidth
+   maxWidth="md"
+   fullScreen={fullScreen}
+ >
+   <IconButton onClick={() => setOpen(false)} sx={{ position: 'absolute', right: 8, top: 8, zIndex: 2, color: 'white' }}>
+     <CloseIcon />
+   </IconButton>
+   <DialogContent sx={{ overflowY: 'auto', maxHeight: '80vh', position: 'relative' }}>
+     <div style={{ position: 'relative' }}>
+       <IconButton 
+         onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex - 1 + selectedProject.images.length) % selectedProject.images.length)} 
+         sx={{ 
+           position: 'absolute', 
+           left: 0, 
+           top: '50%', 
+           transform: 'translateY(-50%)', 
+           zIndex: 2 
+         }}
+       >
+         <ArrowBackIosNewIcon />
+       </IconButton>
+       <CardMedia
+         component="img"
+         image={selectedProject.images[currentImageIndex]}
+         alt={`Image ${currentImageIndex + 1}`}
+       />
+       <IconButton 
+         onClick={() => setCurrentImageIndex((prevIndex) => (prevIndex + 1) % selectedProject.images.length)} 
+         sx={{ 
+           position: 'absolute', 
+           right: 0, 
+           top: '50%', 
+           transform: 'translateY(-50%)', 
+           zIndex: 2 
+         }}
+       >
+         <ArrowForwardIosIcon />
+       </IconButton>
+     </div>
+     <CardContent sx={{ paddingTop: theme.spacing(2) }}>
+       <Typography gutterBottom variant="h5" component="div">
+         {selectedProject.title}
+       </Typography>
+       {selectedProject.fullDesc.map((section, index) => (
+         <div key={index} style={{ marginBottom: '20px' }}>
+           <Typography variant="h6" style={{ color: '#14CEDC' }}>{section.title}</Typography>
+           <Typography variant="body2" color="text.secondary">{section.description}</Typography>
+         </div>
+       ))}
+     </CardContent>
+   </DialogContent>
+ </Dialog>
+ 
       )}
     </Container>
   );
