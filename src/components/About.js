fd7ac24a69754eed6689a69a474a41d951040bb5
Parth <98051471+parthlad9@users.noreply.github.com>
2024-04-17 00:49:49 -0500

    fixed color

--- src/components/About.js
+++ src/components/About.js
@@ -20,22 +20,37 @@ function About() {
         <Grid container spacing={3} justifyContent="center">
           <Grid item xs={12}>
             <Grow in={true} style={{ transformOrigin: '0 0 0' }} timeout={1000}>
-              <Typography paragraph align="center" variant="h6" sx={{ userSelect: 'none', fontFamily: 'Roboto, sans-serif', fontWeight: 'bold', animation: 'dynamic 2s infinite' }}>
+              <Typography paragraph align="center" variant="h6" sx={{
+                userSelect: 'none', 
+                fontFamily: 'Roboto, sans-serif', 
+                fontWeight: 'bold', 
+                animation: 'dynamic 2s infinite'
+              }}>
                 Hello World! I'm Parth Lad, and my expertise lies in web development and Information Technology.
               </Typography>
             </Grow>
-            <Button onClick={handleExpandClick} endIcon={<ExpandMoreIcon />} sx={{ mb: 2 }}>
+            <Button 
+              onClick={handleExpandClick} 
+              endIcon={<ExpandMoreIcon />} 
+              sx={{ 
+                mb: 2,
+                color: '#14CEDC', // Set text color to #14CEDC
+                '&:hover': {
+                  color: '#11B9C5' // Darker shade on hover
+                }
+              }}
+            >
               {expanded ? 'Less About Me' : 'More About Me'}
             </Button>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <Typography paragraph align="left" sx={{
-            fontFamily: 'Roboto, sans-serif',
-            fontWeight: '400',
-            lineHeight: '1.6',
-            marginTop: '1em',
-            marginBottom: '1em',
-            textIndent: '2em'
-          }}>
+              <Typography paragraph align="left" sx={{
+                fontFamily: 'Roboto, sans-serif',
+                fontWeight: '400',
+                lineHeight: '1.6',
+                marginTop: '1em',
+                marginBottom: '1em',
+                textIndent: '2em'
+              }}>
             I bring a strong foundation in front-end technologies, including HTML, CSS, and JavaScript. I have advanced skills in popular frameworks like React and Vue, enhancing my web development capabilities.
 
             In the realm of back-end development, I excel in creating efficient and scalable server-side applications with Node.js and Express. Additionally, I possess extensive experience in database management, utilizing AWS RDS to host SQL databases for reliable and easily accessible data storage solutions.
