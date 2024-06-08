fff1f4ab4883bc29eca70d37385ceab287d5d414
Parth <98051471+parthlad9@users.noreply.github.com>
2024-06-08 18:09:57 -0500

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Box, Button } from '@mui/material';
-import './Home.css';
-import { useTheme } from '../context/ThemeContext'; // Adjust the path as necessary
+import React, { useState, useEffect } from "react";
+import { Typography, Box, Button } from "@mui/material";
+import "./Home.css";
+import { useTheme } from "../context/ThemeContext"; // Adjust the path as necessary
 
 const phrases = ["a Developer", " an IT Specialist"];
 
 function Home({ handleNavItemClicked, navItems }) {
-  const [phrase, setPhrase] = useState('');
+  const [phrase, setPhrase] = useState("");
   const [index, setIndex] = useState(0);
   const [subIndex, setSubIndex] = useState(0);
   const [reverse, setReverse] = useState(false);
@@ -26,19 +26,21 @@ function Home({ handleNavItemClicked, navItems }) {
     }
 
     const timeout = setTimeout(() => {
-      setPhrase((prev) => phrases[index].substring(0, reverse ? prev.length - 1 : prev.length + 1));
+      setPhrase((prev) =>
+        phrases[index].substring(0, reverse ? prev.length - 1 : prev.length + 1)
+      );
       setSubIndex((prev) => prev + (reverse ? -1 : 1));
     }, 150);
 
     return () => clearTimeout(timeout);
   }, [subIndex, index, reverse]);
 
-    // Adjust styles based on the theme
-    const boxStyle = {
-      backgroundColor: themeType === 'dark' ? 'rgb(36, 36, 36)' : 'rgb(255, 255, 255)',
-      color: themeType === 'dark' ? 'rgb(236, 243, 236)' : 'rgb(36, 36, 36)'
-    };
-  
+  // Adjust styles based on the theme
+  const boxStyle = {
+    backgroundColor:
+      themeType === "dark" ? "rgb(36, 36, 36)" : "rgb(255, 255, 255)",
+    color: themeType === "dark" ? "rgb(236, 243, 236)" : "rgb(36, 36, 36)",
+  };
 
   return (
     <Box textAlign="left" p={5} className="console-text" sx={boxStyle}>
@@ -47,14 +49,24 @@ function Home({ handleNavItemClicked, navItems }) {
         <span className="minimize"></span>
         <span className="maximize"></span>
       </div>
-      <Typography variant="h4" gutterBottom>$Hello, my name is Parth Lad</Typography>
-      <Typography variant="h4" gutterBottom>$I am {phrase}</Typography>
-      <Button 
-        variant="contained" 
-        sx={{ mt: 2, backgroundColor: 'rgb(20,206,220)', '&:hover': { backgroundColor: 'rgb(17,185,197)' }}} 
-        onClick={() => handleNavItemClicked(navItems.find(item => item.name === 'Contact'))}
+      <Typography variant="h4" gutterBottom>
+        $Hello, my name is Parth Lad
+      </Typography>
+      <Typography variant="h4" gutterBottom>
+        $I am {phrase}
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{
+          mt: 2,
+          backgroundColor: "rgb(20,206,220)",
+          "&:hover": { backgroundColor: "rgb(17,185,197)" },
+        }}
+        onClick={() =>
+          handleNavItemClicked(navItems.find((item) => item.name === "Contact"))
+        }
       >
-        Hire Me
+        Seeking New Opportunities
       </Button>
     </Box>
   );
